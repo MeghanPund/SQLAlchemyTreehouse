895642543b3f102d43ec7dc18e674b67eee227f1
Meghan Pund <meghan.pund@gmail.com>
2022-02-09 10:19:49 -0500

    imports, added db model

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from models import (Base, session, Book, engine)
+
 # import models
 # main menu - add, search, analysis, exit, view
 # add books to db
@@ -6,3 +8,7 @@
 # search funct
 # data cleaning functions
 # loop that runs programven 
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine) # initialize db
