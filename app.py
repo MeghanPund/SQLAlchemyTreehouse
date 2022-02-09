83f668a0ed4bbb11977d81c8e16bd25b8f5c05f5
Meghan Pund <meghan.pund@gmail.com>
2022-02-09 10:25:28 -0500

    added menu function

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from models import (Base, session, Book, engine)
 
+
+def menu():
+    while True:
+        print('''
+        \nPROGRAMMING BOOKS
+        \r1) Add book
+        \r2) View all books
+        \r3) Search for book
+        \r4) Book analysis
+        \r5) Exit''')
+        choice = input("\nWhat would you like to do? ")
+        if choice in ['1', '2', '3', '4', '5']:
+            return choice
+        else:
+            "Choose an option from above by entering a number from 1-5 and pressing Enter"
 # import models
 # main menu - add, search, analysis, exit, view
 # add books to db
@@ -12,3 +27,4 @@ from models import (Base, session, Book, engine)
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine) # initialize db
+    menu()
